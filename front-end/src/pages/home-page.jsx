86170d0ee30e9fecc7b41f7a99f2ce2978fcd12d
Keren Siebner <kerens969@gmail.com>
2023-01-09 15:55:56 +0200

    first commit

---
+++ front-end/src/pages/home-page.jsx
@@ -0,0 +1,22 @@
+// import { useEffect } from "react"
+// import { useDispatch, useSelector } from "react-redux"
+// import { loadCars } from "../store/car.action.js"
+
+export function HomePage() {
+    // const [count, setCount] = useState(10)
+    // const count = useSelector((storeState) => storeState.appModule.count)
+    // const dispatch = useDispatch()
+
+    // useEffect(() => {
+    //     loadCars()
+    // }, [])
+
+
+    // const imgUrl = 'logo.png'
+    return (
+        <section>
+            <p>hi</p>
+        </section >
+    )
+
+}
---
+++ front-end/src/root-cmp.jsx
@@ -0,0 +1,36 @@
+// import './assets/style/main.css'
+
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+// import { AboutUs } from "./pages/about-us";
+import { HomePage } from './pages/home-page.jsx';
+// import { Provider } from 'react-redux';
+// import { store } from './store/store';
+// import { AppHeader } from './cmps/app-header';
+// import { AppFooter } from './cmps/app-footer';
+// import { ToyIndex } from './pages/toy-index';
+
+
+
+export function App() {
+
+    return (
+        // <Provider store={store}>
+            <Router>
+            {/* //     <section className="main-layout app">
+            //         <AppHeader />
+            //         <main>*/}
+                         <Routes> 
+                            <Route element={<HomePage />} path="/" />
+            {/* //                 <Route element={<AboutUs />} path="/about" />
+            //                 <Route element={<ToyIndex />} path="/toy" />*/}
+
+                        </Routes> 
+            {/* //         </main> */}
+            {/* //         <AppFooter />
+            //     </section> */}
+             </Router>
+        // </Provider>
+    )
+}
+
+
