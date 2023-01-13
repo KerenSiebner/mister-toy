ec66e8384f2e91e1b53216bc410788510c5f0ebd
Keren Siebner <kerens969@gmail.com>
2023-01-13 23:33:23 +0200

    frontend is now using async functions syntax

--- frontend/src/store/toy.action.js
+++ frontend/src/store/toy.action.js
@@ -1,46 +1,42 @@
 import { toyService } from '../services/toy.service.js'
 import { store } from './store.js'
-import { SET_TOYS , SET_IS_LOADING, REMOVE_TOY, ADD_TOY, UPDATE_TOY} from './toy.reducer.js'
+import { SET_TOYS, SET_IS_LOADING, REMOVE_TOY, ADD_TOY, UPDATE_TOY } from './toy.reducer.js'
 
-export function loadToys(filterBy) {
-    console.log('filterBy', filterBy)
-    store.dispatch({ type: SET_IS_LOADING, isLoading: true })
-    return toyService.query(filterBy)
-        .then((toys) => {
-            store.dispatch({ type: SET_TOYS, toys })
-        })
-        .catch(err => {
-            console.log('Had issues loading toys', err)
-            throw err
-        })
-        .finally(()=>{
-            store.dispatch({ type: SET_IS_LOADING, isLoading: false })
-        })
+export async function loadToys(filterBy) {
+    // console.log('filterBy', filterBy)
+    try {
+        store.dispatch({ type: SET_IS_LOADING, isLoading: true })
+        const toys = await toyService.query(filterBy)
+        store.dispatch({ type: SET_TOYS, toys })
+
+    } catch (err) {
+        console.log('Had issues loading toys', err)
+        throw err
+    } finally {
+        store.dispatch({ type: SET_IS_LOADING, isLoading: false })
+    }
 }
 
-export function removeToy(toyId) {
-    return toyService.remove(toyId)
-        .then(() => {
-            store.dispatch({ type: REMOVE_TOY, toyId })
-        })
-        .catch(err => {
-            console.log('Had issues Removing toy', err)
-            throw err
-        })
+export async function removeToy(toyId) {
+    try {
+        await toyService.remove(toyId)
+        store.dispatch({ type: REMOVE_TOY, toyId })
+    } catch (err) {
+        console.log('Had issues Removing toy', err)
+        throw err
+    }
 }
 
-export function saveToy(toy) {
-    console.log('toy', toy)
+export async function saveToy(toy) {
+    // console.log('toy', toy)
     const type = (toy._id) ? UPDATE_TOY : ADD_TOY
-    console.log('type', type)
-    return toyService.save(toy)
-        .then(savedToy => {
-            console.log('savedToy', savedToy)
-            store.dispatch({ type, toy: savedToy })
-            return savedToy
-        })
-        .catch(err => {
-            console.error('Cannot save toy:', err)
-            throw err
-        })
+    // console.log('type', type)
+    try {
+        const savedToy = await toyService.save(toy)
+        store.dispatch({ type, toy: savedToy })
+        return savedToy
+    } catch (err) {
+        console.error('Cannot save toy:', err)
+        throw err
+    }
 }
